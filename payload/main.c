9d59232119da41d5a5954a2ddb45929de413c8ac
Sergi Granell <xerpi.g.12@gmail.com>
2017-07-19 18:40:56 +0200

    Start adding DSI stuff

--- payload/main.c
+++ payload/main.c
@@ -3,6 +3,12 @@
 
 static unsigned int get_cpu_id(void);
 
+#define PRINT_HEX(x) \
+	do { \
+		for (int i = 28; i >= 0; i -= 4) \
+			uart_write(0, "0123456789ABCDEF"[((x) >> i) & 0xF]); \
+	} while (0);
+
 int main(void)
 {
 	if (get_cpu_id() != 0) {
@@ -20,6 +26,12 @@ int main(void)
 	pervasive_reset_exit_uart(0);
 
 	uart_init(0, 115200);
+
+	pervasive_dsi_set_pixeclock(1, 0x223A1C);
+	pervasive_clock_enable_dsi(1, 0xF);
+	pervasive_reset_exit_dsi(1, 7);
+
+	dsi_enable_bus(1);
 
 	while (1) {
 		static int i = 0;
